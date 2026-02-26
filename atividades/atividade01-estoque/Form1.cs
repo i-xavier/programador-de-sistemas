8d33218cfb41eb6e87d6b11d452e5dc3e3c4fa81
i-xavier <igorxavier.49@gmail.com>
2026-02-26 16:21:07 -0300

    atualização atividades - v3

--- atividades/atividade01-estoque/Form1.cs
+++ atividades/atividade01-estoque/Form1.cs
@@ -12,6 +12,8 @@ namespace atividade01
 {
     public partial class Form1 : Form
     {
+        const int LIMITE = 5;
+
         int qtdEstoque = 10;
         
         public Form1()
@@ -25,22 +27,27 @@ namespace atividade01
             try
             {
                 int valor = Convert.ToInt32(txtBox.Text);
+                // int.TryParse(txtBox.Text, out int valor);
 
-               if (qtdEstoque - valor < 0)
-                {
-                    lblNotificacao.Text = "Alerta:Valor de saída maior que o valor do estoque.\nTente outro valor.";
-                }
+                
 
-               else
+               if (valor > 0)
                 {
+                    
+
+                    if (qtdEstoque - valor < 0)
+                    {
+                        lblNotificacao.Text = "Alerta:Valor de saída maior que o valor do estoque.\nTente outro valor.";
+                        return;
+                    }
+
                     qtdEstoque -= valor;
-
 
                     if (qtdEstoque == 0)
                     {
                         lblNotificacao.Text = "Alerta:Estoque vazio. Por favor, reabasteça este produto.";
                     }
-                    else if (qtdEstoque < 5)
+                    else if (qtdEstoque < LIMITE)
                     {
                         lblNotificacao.Text = "Alerta: Baixo estoque.Por favor, reabasteça este produto.";
                     }
@@ -49,10 +56,14 @@ namespace atividade01
 
                     lblQtd.Text = qtdEstoque.ToString();
                 }
+
+               else
+                    lblNotificacao.Text = "Alerta:Valor Inválido.\nTente outra vez.";
+
             }
-            catch
+            catch (Exception ex) 
             {
-                MessageBox.Show("Valor inválido", "Erro");
+                MessageBox.Show("Falha " + ex.Message);
             }
         }
 
@@ -61,21 +72,26 @@ namespace atividade01
             try
             {
                 int valor = Convert.ToInt32(txtBox.Text);
+                // int.TryParse(txtBox.Text, out int valor);
+
+                if (valor > 0) 
+                {
+                    qtdEstoque += valor;
+
+                    lblQtd.Text = qtdEstoque.ToString();
+
+                    if (qtdEstoque >= LIMITE)
+                        lblNotificacao.Text = "Foi adicionado mais " + valor.ToString() + " produto(s).\nEstoque Suficiente";
 
 
-                qtdEstoque += valor;
-
-                lblQtd.Text = qtdEstoque.ToString();
-
-                if(qtdEstoque >= 5)
-                    lblNotificacao.Text = "Foi adicionado mais " + valor.ToString() + " produto(s).\nEstoque Suficiente";
+                }
                 else
-                    lblNotificacao.Text = "Alerta: Baixo estoque.Por favor, reabasteça este produto.";
+                    lblNotificacao.Text = "Alerta:Valor Inválido.\nTente outra vez.";
 
             }
-            catch
+            catch (Exception ex) 
             {
-                MessageBox.Show("Valor inválido", "Erro");
+                MessageBox.Show("Falha " + ex.Message);
             }
         }
     }
